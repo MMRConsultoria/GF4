3554f9aa78a551afa119b56b732b7d029c8aa718
MMR Consultoria <maricelisrossi@gmail.com>
2025-04-23 11:54:52 -0300

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Login | MMR Consultoria")
 
-# Redireciona se já estiver logado
+# Se já estiver logado, volta ao início
 if st.session_state.get("acesso_liberado"):
     st.experimental_rerun()
 
@@ -17,6 +17,9 @@ if st.button("Entrar"):
     if codigo_empresa == "1825" and senha == "1234":
         st.session_state["acesso_liberado"] = True
         st.session_state["empresa"] = codigo_empresa
+
+        # Truque: forçar ir para Home "manualmente" depois do login
+        st.success("Login feito com sucesso!")
         st.experimental_rerun()
     else:
         st.error("❌ Código ou senha incorretos. Tente novamente.")
