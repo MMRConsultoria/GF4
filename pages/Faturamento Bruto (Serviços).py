878378b5ddc4561214524840c09f0684588f65be
MMR Consultoria <maricelisrossi@gmail.com>
2025-04-24 17:42:49 -0300

    Update Faturamento Bruto (Serviços).py

--- pages/Faturamento Bruto (Serviços).py
+++ pages/Faturamento Bruto (Serviços).py
@@ -5,7 +5,6 @@ import pandas as pd
 import numpy as np
 import json
 from io import BytesIO
-from datetime import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
