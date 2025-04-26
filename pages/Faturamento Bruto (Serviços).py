135e83feb4a1436e036c2dd8802b72fb569a50fa
MMR Consultoria <maricelisrossi@gmail.com>
2025-04-25 21:39:35 -0300

    Update Faturamento Bruto (Serviços).py

--- pages/Faturamento Bruto (Serviços).py
+++ pages/Faturamento Bruto (Serviços).py
@@ -10,7 +10,6 @@ def to_excel(df):
     output = BytesIO()
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
         df.to_excel(writer, index=False, sheet_name='Relatorio')
-        writer.save()
     output.seek(0)
     return output
 
