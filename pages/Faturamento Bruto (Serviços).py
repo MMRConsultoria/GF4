e23cf0669a2e7f33512bcfbf9307bc69d593dd42
MMR Consultoria <maricelisrossi@gmail.com>
2025-04-28 23:17:48 -0300

    Update Faturamento Bruto (Serviços).py

--- pages/Faturamento Bruto (Serviços).py
+++ pages/Faturamento Bruto (Serviços).py
@@ -213,7 +213,6 @@ with aba1:
             st.markdown("""
 🔗 [Clique aqui para abrir a **Tabela_Empresa**](https://docs.google.com/spreadsheets/d/13BvAIzgp7w7wrfkwM_MOnHqHYol-dpWiEZBjyODvI4Q/edit?usp=drive_link)
 
-#🔗 [Clique aqui para abrir o **Faturamento Sistema Externo**](https://docs.google.com/spreadsheets/d/1_3uX7dlvKefaGDBUhWhyDSLbfXzAsw8bKRVvfiIz8ic/edit?usp=sharing)
 """)
 
         except Exception as e:
