806451a2f6fb2b04fc6083a298fd5f5a459f3fc6
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-07 18:56:54 -0300

    Update Faturamento Bruto (Serviços).py

--- pages/Faturamento Bruto (Serviços).py
+++ pages/Faturamento Bruto (Serviços).py
@@ -605,10 +605,10 @@ fig_total.update_layout(
 )
 
 # Exibir no Streamlit
-st.subheader("📆 Faturamento Mensal por Mês")
-st.plotly_chart(fig, use_container_width=True)
-
-st.markdown("---")
 
 st.subheader("📊 Faturamento Anual")
 st.plotly_chart(fig_total, use_container_width=True)
+
+st.markdown("---")
+st.subheader("📆 Faturamento Mensal por Mês")
+st.plotly_chart(fig, use_container_width=True)
