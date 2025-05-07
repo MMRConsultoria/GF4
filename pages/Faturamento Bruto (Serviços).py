1cca8af2c5b832420b8363e9d489571bda24af3a
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-07 13:06:53 -0300

    Update Faturamento Bruto (Serviços).py

--- pages/Faturamento Bruto (Serviços).py
+++ pages/Faturamento Bruto (Serviços).py
@@ -470,9 +470,10 @@ with aba4:
 
     st.subheader("📊 Faturamento Real Mensal - 2024 vs 2025 (Lado a Lado com Ano embaixo e Valor em cima)")
 
-    fat_mensal["Nome Mês"] = fat_mensal["Nome Mês"].astype(str)
-    fat_mensal["Ano"] = fat_mensal["Ano"].astype(str)
-    fat_mensal["Mês_Ano"] = fat_mensal["Nome Mês"] + " " + fat_mensal["Ano"]
+    fat_mensal["Mês_Ano"] = fat_mensal["Nome Mês"] + " " + fat_mensal["Ano"]	 
+    #fat_mensal["Nome Mês"] = fat_mensal["Nome Mês"].astype(str)
+    #fat_mensal["Ano"] = fat_mensal["Ano"].astype(str)
+    #fat_mensal["Mês_Ano"] = fat_mensal["Nome Mês"] + " " + fat_mensal["Ano"]
 
 	
     fig = px.bar(
