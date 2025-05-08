c9b9a6937f1ce0f76a62b8bf1541c9ec821688f9
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-08 15:43:52 -0300

    Update logo_sidebar.py

--- logo_sidebar.py
+++ logo_sidebar.py
@@ -4,20 +4,15 @@ def mostrar_logo_cliente():
     st.markdown(
         """
         <style>
+            /* Ajuste na sidebar para adicionar o logo no topo */
             [data-testid="stSidebar"] > div:first-child {
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-            }
-            .logo-container img {
-                max-width: 100px;
-                margin-bottom: 10px;
-                margin-top: 5px;
+                padding-top: 100px;
+                background-image: url("https://raw.githubusercontent.com/MMRConsultoria/mmr-site/main/logo_cliente.png");
+                background-repeat: no-repeat;
+                background-position: top center;
+                background-size: 80px;
             }
         </style>
-        <div class="logo-container">
-            <img src="https://raw.githubusercontent.com/MMRConsultoria/mmr-site/main/logo_cliente.png" alt="Logo Cliente">
-        </div>
         """,
         unsafe_allow_html=True
     )
