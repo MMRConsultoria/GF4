855479542cc08eb060187aef6f950b6c1f7d24de
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-08 15:34:22 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,15 +3,6 @@ import streamlit as st
 
 st.set_page_config(page_title="Portal de Relatórios | MMR Consultoria")
 
-# Logo discreto no canto superior direito
-st.markdown(
-    """
-    <div style="display: flex; justify-content: flex-end; align-items: center; margin-bottom: -30px;">
-        <img src="https://raw.githubusercontent.com/seu_usuario/seu_repositorio/main/logo_fit.png" alt="Logo" height="40">
-    </div>
-    """,
-    unsafe_allow_html=True
-)
 
 # Garante que a chave exista antes de usar
 if "acesso_liberado" not in st.session_state:
