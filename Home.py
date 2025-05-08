a8265ee0dc211c94f316266f90c48d9c6abbff8a
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-08 15:45:25 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,8 +3,15 @@ import streamlit as st
 from logo_sidebar import mostrar_logo_cliente
 
 st.set_page_config(page_title="Portal de Relatórios | MMR Consultoria")
-mostrar_logo_cliente()
 
+
+st.sidebar.markdown(
+    """
+    <div style="text-align: center; padding: 10px 0 30px 0;">
+        <img src="https://raw.githubusercontent.com/MMRConsultoria/mmr-site/main/logo_cliente.png" width="100">
+    </div>
+    """,
+    unsafe_allow_html=True
 
 # Garante que a chave exista antes de usar
 if "acesso_liberado" not in st.session_state:
