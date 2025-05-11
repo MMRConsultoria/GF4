bff148fcdc9c4a07c95c868c5d6a8bccedeb63d6
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-11 19:57:56 -0300

    Update Relatorios Gerenciais.py

--- pages/Relatorios Gerenciais.py
+++ pages/Relatorios Gerenciais.py
@@ -299,4 +299,4 @@ with aba3:
 # 📊 Aba 4 - Gráficos por Loja (em breve)
 # ==========================================================
 with aba4:
-    #st.info("📌 Em breve: Gráficos detalhados por Loja.")
+        #st.info("📌 Em breve: Gráficos detalhados por Loja.")
