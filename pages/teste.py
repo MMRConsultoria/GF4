27215623b4bd8a78000ded015c4a67f8240169b9
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-12 18:33:43 -0300

    Update teste.py

--- pages/teste.py
+++ pages/teste.py
@@ -69,13 +69,6 @@ with aba1:
 # ================================
 
 
-	
-    # Conectar ao Google Sheets
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    credentials_dict = json.loads(st.secrets["GOOGLE_SERVICE_ACCOUNT"])
-    credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
-    gc = gspread.authorize(credentials)
-
     # Carregar dados
     planilha = gc.open("Faturamento Sistema Externo")
     aba = planilha.worksheet("Fat Sistema Externo")
