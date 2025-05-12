edc64abaf3d4f21c5b684715df7b90027e23f4fa
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-12 18:05:42 -0300

    Update RelatGerencial.py

--- pages/RelatGerencial.py
+++ pages/RelatGerencial.py
@@ -86,23 +86,6 @@ aba1, aba2, aba3, aba4 = st.tabs([
 # ================================
 with aba1:
   
-
-
-	
-    # Conectar ao Google Sheets
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    credentials_dict = json.loads(st.secrets["GOOGLE_SERVICE_ACCOUNT"])
-    credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
-    gc = gspread.authorize(credentials)
-
-    # Carregar dados
-    planilha = gc.open("Faturamento Sistema Externo")
-    aba = planilha.worksheet("Fat Sistema Externo")
-    dados = aba.get_all_records()
-    df = pd.DataFrame(dados)
-
-   
-
   # =========================
     # 🧹 Tratamento dos dados
     # =========================
