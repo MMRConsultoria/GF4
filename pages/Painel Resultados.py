0afe0ab64e40daf9f677f8cc8a81918000e9a4ff
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-12 21:23:10 -0300

    Update Painel Resultados.py

--- pages/Painel Resultados.py
+++ pages/Painel Resultados.py
@@ -148,6 +148,7 @@ with aba1:
         lambda row: f"{int(row['Ano'])}       R$ {row['Fat.Real']/1_000_000:,.1f} Mi".replace(",", "."), axis=1
     )
     df_total["Ano"] = df_total["Ano"].astype(str)
+    df_total = df_total.sort_values("Ano")  # Ordena do mais antigo para o mais recente
 
     fig_total = px.bar(
         df_total,
