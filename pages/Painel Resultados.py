4789c3dc6109806ff1ac61ef2f4647f074685878
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-12 21:45:51 -0300

    Update Painel Resultados.py

--- pages/Painel Resultados.py
+++ pages/Painel Resultados.py
@@ -147,7 +147,7 @@ with aba1:
     df_total["AnoTexto"] = df_total.apply(
         lambda row: f"{int(row['Ano'])}       R$ {row['Fat.Real']/1_000_000:,.1f} Mi".replace(",", "."), axis=1
     )
-    df_total["Ano"] = df_total["Ano"].astype(str)
+   
     
    
     df_total = df_total.sort_values("Ano", ascending=False)
@@ -197,7 +197,8 @@ with aba1:
         )
     
     anos_ordenados = df_total["Ano"].tolist()[::-1]  # Inverte a ordem
-    df_total["Ano"] = pd.Categorical(df_total["Ano"], categories=anos_ordenados, ordered=True)
+    df_total["Ano"] = df_total["Ano"].astype(str)
+    df_total["Ano"] = pd.Categorical(df_total["Ano"], categories=[str(a) for a in anos_ordenados], ordered=True)
     
     fig_total.update_layout(
         height=130,
