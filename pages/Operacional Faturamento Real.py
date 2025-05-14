80a4d235e635da72273c3b1a447933e2c15eae8d
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-14 15:38:26 -0300

    Update Operacional Faturamento Real.py

--- pages/Operacional Faturamento Real.py
+++ pages/Operacional Faturamento Real.py
@@ -12,6 +12,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
 import plotly.express as px
+from datetime import date
 
 # ================================
 # 1. Conexão com Google Sheets
