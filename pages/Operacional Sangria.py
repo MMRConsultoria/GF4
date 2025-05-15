47a847b4a3c71c896e556e489728ee30ac6eb2b8
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-15 13:18:02 -0300

    Update Operacional Sangria.py

--- pages/Operacional Sangria.py
+++ pages/Operacional Sangria.py
@@ -6,6 +6,10 @@ from io import BytesIO
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+# 🔒 Bloqueia o acesso caso o usuário não esteja logado
+if not st.session_state.get("acesso_liberado"):
+    st.stop()
+    
 st.set_page_config(page_title="Relatório de Sangria", layout="wide")
 st.markdown("""
     <div style='display: flex; align-items: center; gap: 10px;'>
