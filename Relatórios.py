471b8b9311895244c272a3bc79bb8d9f45c23542
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-15 13:27:47 -0300

    Update Relatórios.py

--- Relatórios.py
+++ Relatórios.py
@@ -1,7 +1,10 @@
 import streamlit as st
+
+st.set_page_config(page_title="MMR Consultoria – Relatórios", layout="centered")
+
 if not st.session_state.get("acesso_liberado"):
     st.stop()
-st.set_page_config(page_title="MMR Consultoria – Relatórios", layout="centered")
+
 
 st.title("📊 Relatórios – MMR Consultoria")
 st.markdown("Selecione um relatório no menu lateral à esquerda.")
