1c7dda1453dbf8146e5a5a093ef4d2d10313405b
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-20 13:46:57 -0300

    Update Painel Resultados.py

--- pages/Painel Resultados.py
+++ pages/Painel Resultados.py
@@ -264,7 +264,7 @@ with aba3:
 # Aba 4: Analise Lojas
 # ================================
 with aba4:
-
+    from datetime import datetime
     st.markdown("""
     <style>
     /* Zera espaçamentos verticais padrão */
