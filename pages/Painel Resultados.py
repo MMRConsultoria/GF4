f3b1917477e3e2ef01b058d6f99eab4ce33eddc2
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-21 22:54:12 -0300

    Update Painel Resultados.py

--- pages/Painel Resultados.py
+++ pages/Painel Resultados.py
@@ -776,13 +776,12 @@ with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
             else:
                 linha_tipo.append("")
 
-        for col_num, val in enumerate(linha_tipo):
-            if isinstance(val, (int, float)) and not pd.isna(val):
-                worksheet.write_number(linha, col_num, val, subtotal_format)
-            else:
-                worksheet.write(linha, col_num, str(val), subtotal_format)
-
-        linha += 1
+    for col_num, val in enumerate(linha_tipo):
+        if isinstance(val, (int, float)) and not pd.isna(val):
+            worksheet.write_number(linha, col_num, val, subtotal_format)
+        else:
+            worksheet.write(linha, col_num, str(val), subtotal_format)
+    linha += 1
 
     # 🔥 Total Geral
     linha_totalgeral = ["Total Geral", ""]
