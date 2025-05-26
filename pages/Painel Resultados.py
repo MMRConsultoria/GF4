e29b3e54831449d57e1ddef4c0c970d1ac42cc2d
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-26 12:18:30 -0300

    Update Painel Resultados.py

--- pages/Painel Resultados.py
+++ pages/Painel Resultados.py
@@ -579,7 +579,7 @@ with aba4:
 
     # 🔥 Garante que traz apenas lojas ativas
     todas_lojas = df_empresa[
-        df_empresa["Lojas Ativas"].str.lower().str.strip() == "sim"
+        df_empresa["Lojas Ativas"].str.lower().str.strip() == "Ativa"
     ][["Loja", "Grupo", "Tipo"]].drop_duplicates()
 
     # 🔗 Faz o pivot dos dados
