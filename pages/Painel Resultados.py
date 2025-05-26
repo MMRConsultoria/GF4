c00e1d8ac443b5b0b9c497b4323a01d7b6eb8f8d
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-26 13:39:09 -0300

    Update Painel Resultados.py

--- pages/Painel Resultados.py
+++ pages/Painel Resultados.py
@@ -421,28 +421,28 @@ with aba4:
          agrupamento = st.radio(" ", ["Ano", "Mês", "Dia"], horizontal=True, key="agrup_aba4")
 
 
-     if modo_visao == "Por Loja":
-        lojas_com_venda = df_filtrado["Loja"].unique()
-        lojas_sem_venda = todas_lojas[~todas_lojas["Loja"].isin(lojas_com_venda)]
+if modo_visao == "Por Loja":
+    lojas_com_venda = df_filtrado["Loja"].unique()
+    lojas_sem_venda = todas_lojas[~todas_lojas["Loja"].isin(lojas_com_venda)]
 
-        if not lojas_sem_venda.empty:
-            df_sem_venda = pd.DataFrame({
-                "Loja": lojas_sem_venda["Loja"],
-                "Grupo": lojas_sem_venda["Grupo"],
-                "Tipo": lojas_sem_venda["Tipo"],
-                "Fat.Total": 0,
-                "Fat.Real": 0,
-                "Data": pd.NaT,
-                "Ano": None,
-                "Mês Num": None,
-                "Mês Nome": None,
-                "Mês": None,
-                "Dia": None,
-                "Agrupador": None,
-                "Ordem": None
-            })
+    if not lojas_sem_venda.empty:
+        df_sem_venda = pd.DataFrame({
+            "Loja": lojas_sem_venda["Loja"],
+            "Grupo": lojas_sem_venda["Grupo"],
+            "Tipo": lojas_sem_venda["Tipo"],
+            "Fat.Total": 0,
+            "Fat.Real": 0,
+            "Data": pd.NaT,
+            "Ano": None,
+            "Mês Num": None,
+            "Mês Nome": None,
+            "Mês": None,
+            "Dia": None,
+            "Agrupador": None,
+            "Ordem": None
+        })
 
-            df_filtrado = pd.concat([df_filtrado, df_sem_venda], ignore_index=True)
+        df_filtrado = pd.concat([df_filtrado, df_sem_venda], ignore_index=True)
 
 
 
