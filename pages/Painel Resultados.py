5d2df95d34af7172a39319058ec5a1c65da8a893
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-28 17:18:45 -0300

    Update Painel Resultados.py

--- pages/Painel Resultados.py
+++ pages/Painel Resultados.py
@@ -734,7 +734,7 @@ elif modo_visao == "Por Grupo":
         tabela_exportar["Tipo"] = df_empresa.groupby("Grupo")["Tipo"].agg(lambda x: x.mode().iloc[0] if not x.mode().empty else None).reindex(tabela_exportar["Grupo"]).values
  
     st.write("🚧 Debug Grupo", tabela_exportar)
-    st.write("📄 df_empresa", df_empresa)
+    #st.write("📄 df_empresa", df_empresa)
 
 # 🔥 Define qual coluna usar para o acumulado (Bruto ou Real)
 coluna_acumulado = "Fat.Total"  # 🔥 Para Acumulado no mês pelo Bruto (com gorjeta)
