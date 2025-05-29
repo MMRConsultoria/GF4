2c0dc51ee984c8452bbd34e14216d2e90b150ca1
MMR Consultoria <maricelisrossi@gmail.com>
2025-05-29 16:42:41 -0300

    Update Painel Resultados.py

--- pages/Painel Resultados.py
+++ pages/Painel Resultados.py
@@ -485,14 +485,16 @@ with aba4:
 
         if grupos_faltando:
             df_faltando = pd.DataFrame({
-                "Grupo": grupos_faltando,
-                "Data": data_selecionada,
-                "Fat.Total": 0,
-                "Fat.Real": 0,
-                "Serv/Tx": 0,
-                "Ticket": 0,
-            })
-            df_filtrado = pd.concat([df_filtrado, df_faltando], ignore_index=True)
+            "Grupo": grupos_faltando,
+            "Data": data_selecionada,
+            "Fat.Total": 0,
+            "Fat.Real": 0,
+            "Serv/Tx": 0,
+            "Ticket": 0,
+            "Agrupador": data_selecionada.strftime("%d/%m/%Y"),
+            "Ordem": data_selecionada
+        })
+        df_filtrado = pd.concat([df_filtrado, df_faltando], ignore_index=True)
 
 
 
