c57cc0c4c3828c31e94d62618a1cfb4966ce4764
MMR Consultoria <maricelisrossi@gmail.com>
2025-06-03 13:16:26 -0300

    Update Painel Resultados.py

--- pages/Painel Resultados.py
+++ pages/Painel Resultados.py
@@ -1,7 +1,6 @@
 # pages/PainelResultados.py
 import streamlit as st
 st.set_page_config(page_title="Vendas Diarias", layout="wide")  # ✅ Escolha um título só
-
 import streamlit as st
 import pandas as pd
 import numpy as np
