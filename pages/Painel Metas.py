8b42d66a4db655b107e20b95e089885b148cf084
MMR Consultoria <maricelisrossi@gmail.com>
2025-06-30 09:37:19 -0300

    Update Painel Metas.py

--- pages/Painel Metas.py
+++ pages/Painel Metas.py
@@ -326,7 +326,7 @@ with aba1:
                 estilo.append("background-color: #FFE699;")
 
             elif "Lojas:" in str(row["Loja"]):
-                estilo.append("background-color: #3387e0; color: white;")  # <-- ALTERADO AQUI
+                estilo.append("background-color: #cce7fc; color: white;")  # <-- ALTERADO AQUI
             elif "Lojas:" in str(row["Loja"]):
                 # 💡 Aqui aplicamos verde/vermelho apenas no modo de grupo e na coluna % Atingido
                 if (
