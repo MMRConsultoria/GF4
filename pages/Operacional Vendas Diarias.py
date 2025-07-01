2d5332fd02f3c268786d080c3a445752aa2fe11e
MMR Consultoria <maricelisrossi@gmail.com>
2025-06-30 23:36:53 -0300

    Update Operacional Vendas Diarias.py

--- pages/Operacional Vendas Diarias.py
+++ pages/Operacional Vendas Diarias.py
@@ -273,26 +273,24 @@ with aba1:
                 st.markdown(mensagem, unsafe_allow_html=True)
             else:
                 st.success("✅ Todas as empresas foram localizadas na Tabela_Empresa!")
+            
+                # 📥 Botão de Download do Excel só aparece se não tiver empresas pendentes
+                def to_excel(df):
+                    output = BytesIO()
+                    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+                        df.to_excel(writer, index=False, sheet_name='Faturamento Servico')
+                    output.seek(0)
+                    return output
+            
+                excel_data = to_excel(df_final)
+            
+                st.download_button(
+                    label="📥 Baixar Relatório Excel",
+                    data=excel_data,
+                    file_name="faturamento_servico.xlsx",
+                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                )
 
-        except Exception as e:
-            st.error(f"❌ Erro ao processar o arquivo: {e}")
-    # 📥 Botão de Download do Excel diretamente na Aba 1
-    if 'df_final' in st.session_state:
-        def to_excel(df):
-            output = BytesIO()
-            with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-                df.to_excel(writer, index=False, sheet_name='Faturamento Servico')
-            output.seek(0)
-            return output
-
-        excel_data = to_excel(st.session_state.df_final)
-
-        st.download_button(
-            label="📥 Baixar Relatório Excel",
-            data=excel_data,
-            file_name="faturamento_servico.xlsx",
-            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        )
 
 
 # =======================================
