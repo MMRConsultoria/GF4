67b2de9f8eb4391b20442569b87ef0c913f285d6
MMR Consultoria <maricelisrossi@gmail.com>
2025-07-07 16:17:58 -0300

    Update Painel Metas.py

--- pages/Painel Metas.py
+++ pages/Painel Metas.py
@@ -258,11 +258,18 @@ with aba2:
     ordem_meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
 
     anos_disponiveis = sorted(df_anos["Ano"].unique())
-    ano_selecionado = st.selectbox("Selecione o Ano:", anos_disponiveis, index=anos_disponiveis.index(ano_atual) if ano_atual in anos_disponiveis else 0)
-    mes_selecionado = st.selectbox("Selecione o Mês:", ordem_meses, index=ordem_meses.index(mes_atual) if mes_atual in ordem_meses else 0)
+    col1, col2, col3 = st.columns(3)
 
-    df_anos_filtrado = df_anos[(df_anos["Ano"] == ano_selecionado) & (df_anos["Mês"] == mes_selecionado)].copy()
-    df_metas_filtrado = df_metas[(df_metas["Ano"] == ano_selecionado) & (df_metas["Mês"] == mes_selecionado)].copy()
+    with col1:
+        ano_selecionado = st.selectbox("Ano:", anos_disponiveis, index=anos_disponiveis.index(ano_atual))
+    
+    with col2:
+        mes_selecionado = st.selectbox("Mês:", ordem_meses, index=ordem_meses.index(mes_atual))
+    
+    with col3:
+        modo_visao = st.selectbox("Visão:", ["Por Loja", "Por Grupo"])
+        df_anos_filtrado = df_anos[(df_anos["Ano"] == ano_selecionado) & (df_anos["Mês"] == mes_selecionado)].copy()
+        df_metas_filtrado = df_metas[(df_metas["Ano"] == ano_selecionado) & (df_metas["Mês"] == mes_selecionado)].copy()
 
     import calendar
     ultima_data_realizado_dt = df_anos_filtrado["Data"].max()
