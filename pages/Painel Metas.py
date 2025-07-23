23873163b8c20657e8b94116d83449389e4090f6
MMR Consultoria <maricelisrossi@gmail.com>
2025-07-23 16:58:52 -0300

    Update Painel Metas.py

--- pages/Painel Metas.py
+++ pages/Painel Metas.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title="Vendas Diarias", layout="wide")
 
 import pandas as pd
 import numpy as np
+import re
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
