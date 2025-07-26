4530f0dbdd033226881ad81386fc6472a5930e6c
MMR Consultoria <maricelisrossi@gmail.com>
2025-07-26 16:12:23 -0300

    Update Relatorios.py

--- pages/Relatorios.py
+++ pages/Relatorios.py
@@ -463,10 +463,12 @@ with aba3:
         def estilo_linha(row):
             if row["Grupo"] == "TOTAL":
                 return ["background-color: #f0f0f0; font-weight: bold"] * len(row)
+            elif row["Grupo"] == "Lojas Ativas":
+                return ["background-color: #eeeeee; font-style: italic"] * len(row)  # ⛔ aqui está o destaque
             else:
                 return ["" for _ in row]
         return df.style.apply(estilo_linha, axis=1)
-        
+            
     # Exibição
     st.dataframe(
         aplicar_estilo(df_formatado),
