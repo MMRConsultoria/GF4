1c74a3df37f63c05433208dcb5126fb01448786c
MMR Consultoria <maricelisrossi@gmail.com>
2025-08-26 22:15:40 -0300

    Update Relatorios.py

--- pages/Relatorios.py
+++ pages/Relatorios.py
@@ -24,7 +24,20 @@ from calendar import monthrange
 # 🔒 Bloqueia o acesso caso o usuário não esteja logado
 if not st.session_state.get("acesso_liberado"):
     st.stop()
+import streamlit as st
 
+# =====================================
+# CSS para esconder barra de botões do canto superior direito
+# =====================================
+st.markdown("""
+    <style>
+        [data-testid="stToolbar"] {
+            visibility: hidden;
+            height: 0%;
+            position: fixed;
+        }
+    </style>
+""", unsafe_allow_html=True)
 # ================================
 # 1. Conexão com Google Sheets
 # ================================
